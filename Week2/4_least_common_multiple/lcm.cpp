1cf98787acec89dc7024767af957c2a00eb27804
Haitham Khedr <Haithamkhedr@gmail.com>
2018-03-24 14:52:50 +0200

    Implemented lcm_fast using gcd

--- Week2/4_least_common_multiple/lcm.cpp
+++ Week2/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,32 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int gcd_fast(int a, int b){
+    int gcd = 1;
+    if(b == 0){
+        gcd = a;
+    }
+    else{
+        a = a % b;
+        gcd = gcd_fast(b,a);
+    }
+    
+    return gcd;
+}
+
+long long lcm_fast(int a, int b){
+    if(a>b){
+        a = a / gcd_fast(a,b);
+    }
+    else{
+        b = b / gcd_fast(b,a);
+    }
+    return a*b;
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
