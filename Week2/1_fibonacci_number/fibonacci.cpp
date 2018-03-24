6e8a981948784849604448dfa5d5e07b0944ce77
Haitham Khedr <Haithamkhedr@gmail.com>
2018-03-24 15:24:35 +0200

    Optimized memory usage for Fibonacci

--- Week2/1_fibonacci_number/fibonacci.cpp
+++ Week2/1_fibonacci_number/fibonacci.cpp
@@ -14,31 +14,37 @@
 // 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
 //    and submit it to the grader.
 
-int fibonacci_naive(int n) {
+unsigned long long fibonacci_naive(int n) {
+    
     if (n <= 1)
         return n;
 
-    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+    return (unsigned long long) (fibonacci_naive(n - 1) + fibonacci_naive(n - 2));
 }
 
-int fibonacci_fast(int n) {
+unsigned long long fibonacci_fast(int n) {
     // write your code here
-    // 0 <= n <= 45
-    unsigned long long fib[46];
-    fib[0] = 0;
-    fib[1] = 1;
-    
-    for(int i = 2; i <= n; ++i){
-        fib[i] = fib[i - 1] + fib[i - 2];
+    if(n <= 1)
+        return n;
+    unsigned long long prev = 0;
+    unsigned long long curr = 1;
+    for(int i = 0; i < n-1; ++i){
+        unsigned long long temp_prev  = prev;
+        prev = curr;
+        curr = temp_prev + curr;
     }
-    return fib[n];
+    return curr;
 }
 
 void test_solution() {
     assert(fibonacci_fast(3) == 2);
     assert(fibonacci_fast(10) == 55);
     for (int n = 0; n < 20; ++n)
-        assert(fibonacci_fast(n) == fibonacci_naive(n));
+        if(fibonacci_fast(n) != fibonacci_naive(n)){
+            std::cout<<"fibonacci_fast("<< n <<") : "<< fibonacci_fast(n)<<std::endl;
+            std::cout<<"fibonacci_naive("<< n <<") : "<< fibonacci_naive(n)<<std::endl;
+            break;
+        }
 }
 
 int main() {
