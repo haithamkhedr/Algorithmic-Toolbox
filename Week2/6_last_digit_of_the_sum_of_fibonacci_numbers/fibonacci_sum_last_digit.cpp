5bd8b89d8e2f46275f5275452781e0cb8c876a9a
Haitham Khedr <Haithamkhedr@gmail.com>
2018-03-25 18:07:59 +0200

    Implemented Fibonacci sum mod 10

--- Week2/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ Week2/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-int fibonacci_sum_naive(long long n) {
+unsigned long long fibonacci_sum_naive(long long n) {
     if (n <= 1)
         return n;
 
-    long long previous = 0;
-    long long current  = 1;
-    long long sum      = 1;
+    unsigned long long previous = 0;
+    unsigned long long current  = 1;
+    unsigned long long sum      = 1;
 
     for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
+        unsigned long long tmp_previous = previous;
         previous = current;
         current = tmp_previous + current;
         sum += current;
@@ -18,8 +18,29 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+long long get_fibonacci_sum_fast(long long n){
+    
+    //Pisano period for mod 10 is 60
+    if(n <= 1)
+        return n;
+    int period = 60;
+    return (fibonacci_sum_naive((n-1) % period) + fibonacci_sum_naive((n-2) % period) + 1) % 10 ;
+}
+
+void test_solution() {
+    
+    for (int n = 2; n < 91; ++n){
+        if(get_fibonacci_sum_fast(n) != fibonacci_sum_naive(n)) {
+            std::cout<<"get_fibonacci_sum_fast("<< n<<") : "<< get_fibonacci_sum_fast(n)<<std::endl;
+            std::cout<<"fibonacci_sum_naive("<< n<<") : "<< fibonacci_sum_naive(n)<<std::endl;
+            break;
+        }
+    }
+    
+}
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << get_fibonacci_sum_fast(n);
+//    test_solution();
 }
