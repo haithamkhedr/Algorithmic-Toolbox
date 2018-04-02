b86b8114202fa198957a42c10d7db83fd4ee207a
Haitham Khedr <Haithamkhedr@gmail.com>
2018-04-02 19:05:39 +0200

    Efficient implementation of largest_number O(n log(n) )

--- Week3/6_maximum_salary/largest_number.cpp
+++ Week3/6_maximum_salary/largest_number.cpp
@@ -10,24 +10,15 @@ bool compare(const string & a,const string &b){
     return (std::stoi(a + b) > std::stoi(b+a));
 }
 string largest_number(vector<string> a) {
-  //write your code here
-  std::stringstream ret;
-//    std::sort(a.begin(), a.end(),compare);
-  for (size_t i = 0; i < a.size(); i++) {
-      string max = "-1";
-      size_t max_idx = -1;
-      for(size_t j = 0; j < a.size(); ++j){
-          if(compare(a[j],max)){
-              max = a[j];
-              max_idx = j;
-          }
-      }
-      a[max_idx] = "-1";
-      ret << max;
-  }
-  string result;
-  ret >> result;
-  return result;
+    //write your code here
+    std::stringstream ret;
+    std::sort(a.begin(), a.end(),compare);
+    for (size_t i = 0; i < a.size(); i++) {
+      ret << a[i];
+    }
+    string result;
+    ret >> result;
+    return result;
 }
 
 int main() {
