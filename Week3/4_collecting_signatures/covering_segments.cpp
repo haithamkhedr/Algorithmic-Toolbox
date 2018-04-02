314dedf8ed2cb64393935cd16a8974befff1fb6c
Haitham Khedr <Haithamkhedr@gmail.com>
2018-04-02 12:19:30 +0200

    Implemented covering segments

--- Week3/4_collecting_signatures/covering_segments.cpp
+++ Week3/4_collecting_signatures/covering_segments.cpp
@@ -5,21 +5,32 @@
 
 using std::vector;
 
+
+
 struct Segment {
   int start, end;
 };
+template class std::vector<int>;
+template class std::vector<Segment>;
 bool compare(const Segment& s1, const Segment& s2){
     return (s1.start < s2.start);
 }
 
 vector<int> optimal_points(vector<Segment> &segments) {
-  vector<int> points;
-  //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
-  }
-  return points;
+    vector<int> points;
+    //write your code here
+    std::sort(segments.begin(), segments.end(), compare);
+    for (size_t i = 0; i < segments.size(); ++i) {
+        if(i !=0 && points.back() >= segments[i].start)
+            continue;
+        else if(segments[i].end > segments[i+1].start){
+            points.push_back(std::min(segments[i].end, segments[i+1].end));
+            }
+        else{
+            points.push_back(segments[i].end);
+        }
+    }
+    return points ;
 }
 
 int main() {
@@ -29,6 +40,7 @@ int main() {
   for (size_t i = 0; i < segments.size(); ++i) {
     std::cin >> segments[i].start >> segments[i].end;
   }
+    
   vector<int> points = optimal_points(segments);
   std::cout << points.size() << "\n";
   for (size_t i = 0; i < points.size(); ++i) {
