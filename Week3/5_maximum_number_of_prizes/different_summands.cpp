69d5b7cec8b00fea44dbb2aa47a805bb2f721856
Haitham Khedr <Haithamkhedr@gmail.com>
2018-04-02 16:08:55 +0200

    Implemented optimal summands

--- Week3/5_maximum_number_of_prizes/different_summands.cpp
+++ Week3/5_maximum_number_of_prizes/different_summands.cpp
@@ -5,7 +5,20 @@ using std::vector;
 
 vector<int> optimal_summands(int n) {
   vector<int> summands;
+    summands.push_back(1);
   //write your code here
+    long long sum=1;
+    for(int i=2; sum < n; ++i){
+        sum += i;
+        if(sum > n){
+            summands.back() += (n - (sum - i));
+            break;
+        }
+        else{
+            summands.push_back(i);
+        }
+        
+    }
   return summands;
 }
 
