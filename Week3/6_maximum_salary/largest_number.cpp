083cb366530d250b4f28819333d345a84d7dfa9f
Haitham Khedr <Haithamkhedr@gmail.com>
2018-04-02 17:32:06 +0200

    Implement custom compare function

--- Week3/6_maximum_salary/largest_number.cpp
+++ Week3/6_maximum_salary/largest_number.cpp
@@ -6,10 +6,21 @@
 
 using std::vector;
 using std::string;
-
+bool compare(const string & a,const string &b){
+    bool ret = false;
+    if(a[0] > b[0]){
+        ret = true;
+    }
+    else if( a[0] == b[0] && a[0] > b[1]){
+        ret = true;
+    }
+    return ret;
+}
 string largest_number(vector<string> a) {
   //write your code here
   std::stringstream ret;
+    std::sort(a.begin(), a.end(),compare);
+
   for (size_t i = 0; i < a.size(); i++) {
     ret << a[i];
   }
