e998d837317e00e4164474e3a1009164a561e59d
Haitham Khedr <Haithamkhedr@gmail.com>
2018-04-12 13:40:32 +0200

    Implemented Binary search

--- Week4/1_binary_search/binary_search.cpp
+++ Week4/1_binary_search/binary_search.cpp
@@ -4,14 +4,31 @@
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
+int binary_search_recursive(const vector<int> &a, int x, int left, int right) {
+    
+    int ret = -1;
   //write your code here
+    if(left <= right){
+        
+        int mid = (left + right) / 2;
+        
+        if(a[mid] == x){
+            ret = mid;
+        }
+        else if( x < a[mid] ){
+            ret = binary_search_recursive(a, x, left, mid-1);
+        }
+        else{
+            ret = binary_search_recursive(a, x, mid+1, right);
+        }
+        
+    }
+    return ret;
 }
 
 int linear_search(const vector<int> &a, int x) {
   for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
+    if (a[i] == x) return (int)i;
   }
   return -1;
 }
@@ -31,6 +48,8 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+    //std::cout << linear_search(a, b[i]) << ' ';
+      std::cout << binary_search_recursive(a, b[i],0,(int) (b.size() -1) ) << ' ';
+
   }
 }
