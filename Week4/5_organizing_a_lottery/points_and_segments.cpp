46f2bd878727819d7665a2bce82d18d95ffec956
Haitham Khedr <Haithamkhedr@gmail.com>
2018-04-20 18:03:39 +0200

    Fix array copies in merge sort

--- Week4/5_organizing_a_lottery/points_and_segments.cpp
+++ Week4/5_organizing_a_lottery/points_and_segments.cpp
@@ -6,8 +6,8 @@ using std::vector;
 #define SMALLER_THAN_OR_EQ 0
 #define LARGER_THAN_OR_EQ 1
 //template class std::vector<int>;
-void merge(vector<int>& arr, size_t left, size_t mid, size_t right){
-    vector<int> arr_cp(arr);
+void merge(vector<int>& arr, int left, int mid, int right){
+    vector<int> arr_cp(arr.begin()+left, arr.begin() + right + 1);
     size_t i=0, p1=0 ,p2=mid+1;
     while( (p1 <= mid) || (p2 <= right) ){
         if(p1 > mid){
@@ -26,22 +26,22 @@ void merge(vector<int>& arr, size_t left, size_t mid, size_t right){
     }
 }
 
-void merge_sort(vector<int>& arr, size_t left, size_t right){
+void merge_sort(vector<int>& arr, int left, int right){
     
     if(left >= right)
         return;
-    size_t mid = (left + right)/2 ;
+    int mid = (left + right)/2 ;
     merge_sort(arr, left, mid);
     merge_sort(arr, mid+1, right);
     merge(arr,left,mid,right);
 }
 
-size_t binary_search_modified(vector<int> arr, int x, size_t left, size_t right, int mode){
+size_t binary_search_modified(const vector<int>& arr, int x, int left, int right, int mode){
     
     if(left > right)
         return left - 1;
     
-    size_t mid = (left + right) / 2;
+    int mid = (left + right) / 2;
     if (mode == SMALLER_THAN_OR_EQ){
         if( x <  arr[mid]){
             return binary_search_modified(arr, x, left, mid-1,mode);
@@ -68,8 +68,8 @@ vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int
     merge_sort(starts,0,n-1);
     merge_sort(ends,0,n-1);
     for (int i = 0; i < cnt.size(); ++i) {
-        size_t l = binary_search_modified(starts,points[i], 0, n-1, SMALLER_THAN_OR_EQ) + 1;
-        size_t r = n - (binary_search_modified(ends,points[i], 0, n-1, LARGER_THAN_OR_EQ) + 1);
+        int l = binary_search_modified(starts,points[i], 0, n-1, SMALLER_THAN_OR_EQ) + 1;
+        int r = n - (binary_search_modified(ends,points[i], 0, n-1, LARGER_THAN_OR_EQ) + 1);
         cnt[i] = (int)(l + r - n);
     }
   return cnt;
@@ -86,6 +86,7 @@ vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<in
 }
 //#define TEST_SORT
 //#define TEST_B_SEARCH
+//#define GEN_LARGE_INPUT
 
 int main() {
 #ifdef TEST_SORT
@@ -104,6 +105,29 @@ int main() {
     std::cout<<binary_search_modified(b, 3, 0, b.size()-1,SMALLER_THAN_OR_EQ)<< std::endl;
     std::cout<<binary_search_modified(b, 3, 0, b.size()-1,LARGER_THAN_OR_EQ)<< std::endl;
 
+    
+#elif defined (GEN_LARGE_INPUT)
+    int n = 10000 , m =10000;
+    vector<int> starts(n), ends(n);
+    vector<int> points(m);
+
+    for (size_t i = 0; i < starts.size(); i++) {
+        starts[i] = rand()% 100000000;
+        ends[i] = rand()% 100000000;
+    }
+    for (size_t i = 0; i < points.size(); i++) {
+        points[i] = rand()% 100000000;
+    }
+    auto begin = std::clock();
+    vector<int> cnt1 = naive_count_segments(starts, ends, points);
+    auto diff = std::clock() - begin;
+    std::cout<<std::endl<< (float)diff/CLOCKS_PER_SEC <<std::endl;
+
+    begin = std::clock();
+    vector<int> cnt2 = fast_count_segments(starts, ends, points);
+    diff = std::clock() - begin;
+    std::cout<<std::endl<< (float)diff/CLOCKS_PER_SEC<<std::endl;
+
 #else
   int n, m;
   std::cin >> n >> m;
@@ -116,7 +140,7 @@ int main() {
     std::cin >> points[i];
   }
   //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
+  vector<int> cnt = fast_count_segments(starts, ends, points);
   for (size_t i = 0; i < cnt.size(); i++) {
     std::cout << cnt[i] << ' ';
   }
