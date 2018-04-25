10a1a0aa5670f00420fc78879c807bc62a2b8c7e
Haitham Khedr <Haithamkhedr@gmail.com>
2018-04-25 17:16:19 +0200

    Implemented longest common subseq 2

--- Week5/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ Week5/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -2,12 +2,27 @@
 #include <vector>
 
 using std::vector;
+template class std::vector< std::vector<int> >;
 
 int lcs2(vector<int> &a, vector<int> &b) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+    //write your code here
+    int n1 = a.size() + 1;
+    int n2 = b.size() + 1;
+    std::vector< std::vector<int> > lcs(n1, std::vector<int>(n2,0));
+    for (size_t i = 1; i < n1; ++i) {
+        for (size_t j = 1; j < n2; ++j) {
+            int d_ins = lcs[i-1][j];
+            int d_del = lcs[i][j-1];
+            int d_match = lcs[i-1][j-1];
+            if(a[i-1] == b[j-1]){
+                d_match += 1;
+            }
+            lcs[i][j] = std::max(d_match, std::max(d_ins,d_del));
+        }
+    }
+    
+    return lcs[n1 -1][n2-1];
 }
-
 int main() {
   size_t n;
   std::cin >> n;
